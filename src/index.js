68f0ed9279c2917db69ebae890ca810984edbda0
alibhojani92 <alibhojani92@gmail.com>
2026-02-10 02:23:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
-import { parseUpdate } from "./utils/parser"
-import { handleCommand } from "./handlers/command"
-import { handleCallback } from "./handlers/callback"
-
 export default {
   async fetch(request, env) {
-   console.log("🔥 INDEX.JS LOADED");
-    if (request.method !== "POST") {
-      return new Response("OK")
-    }
-
-    let update
-    try {
-      update = await request.json()
-    } catch {
-      return new Response("OK")
-    }
-
-    const ctx = parseUpdate(update)
-    if (!ctx) return new Response("OK")
-
-    // 🔥 ALWAYS allow commands
-    if (ctx.type === "message") {
-      await handleCommand(ctx, env)
-      return new Response("OK")
-    }
-
-    if (ctx.type === "callback") {
-      await handleCallback(ctx, env)
-      return new Response("OK")
-    }
-
-    return new Response("OK")
+    return new Response("HELLO FROM WORKER", {
+      headers: { "content-type": "text/plain" }
+    })
   }
 }
